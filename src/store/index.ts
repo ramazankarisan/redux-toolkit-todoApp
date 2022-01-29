3f5ee414299a189723e9c2c2a95bc9bb06841f8a
ramazankarisan <rkarisan25@gmail.com>
2022-01-29 16:55:45 +0100

    connect userSlice to the store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import todoSlice from "../features/todoSlice";
+import userSlice from "../features/userSlice";
 
 const store = configureStore({
   reducer:{
-    todos: todoSlice
+    todos: todoSlice,
+    user:userSlice
   }
 })
 
