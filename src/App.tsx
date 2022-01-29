4eeccddda874d60054b89b821582878f33e44627
ramazankarisan <rkarisan25@gmail.com>
2022-01-29 16:56:26 +0100

    simply implement to the landing paage fetched data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import  { add, remove, toggleCompleted } from "./features/todoSlice";
+import { fetchUser } from "./features/userSlice";
 import { useAppDispatch, useAppSelector } from "./store";
 
 
 function App() {
   const todos = useAppSelector(state => state.todos)
+  const user = useAppSelector(state => state.user)
   const [title, setTitle] = useState("");
   const dispatch = useAppDispatch()
 
@@ -19,6 +21,8 @@ function App() {
   const toogle = (id:string) => {
     dispatch(toggleCompleted(id))
   }
+  const currentUser = user.data && user.data.results[0];
+  
   return (
     <div className="App">
       <h1>Redux-Toolkit Exercise</h1>
@@ -33,6 +37,16 @@ function App() {
             <span>{todo.title}</span>
             </li>))}
       </ul>
+      <div>
+        <button onClick={()=> dispatch(fetchUser())} >fetch user</button>
+        {user.loading && "loading..."}
+         {user.error && user.error}
+         {currentUser && <div>
+           <p>name: {currentUser.name.title} {currentUser.name.first} {currentUser.name.last}</p>
+           <p>avatar : <img src={currentUser.picture.large} alt="" /></p>
+           email: {currentUser.email}
+           </div>}
+      </div>
     </div>
   );
 }
